c13151df5b6b6c665c9c11e3798a94c3774d9d95
Aashik Ramees <aashiknov@gmail.com>
2026-05-14 22:07:34 +0530

    Initial commit

--- src/main/java/com/tutorbooking/HomeTutorApplication.java
+++ src/main/java/com/tutorbooking/HomeTutorApplication.java
@@ -1,4 +1,13 @@
 package com.tutorbooking;
 
+package com.tutorbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HomeTutorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HomeTutorApplication.class, args);
+    }
 }
