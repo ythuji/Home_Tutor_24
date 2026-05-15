ebf3c554c8e95e3e72cabb6b725fc69fe4c9cbbe
chaveenfernando <chaveenfernando@gmail.com>
2026-05-15 12:10:44 +0530

    Added SpringBoot main application class

--- src/main/java/com/tutorbooking/HomeTutorApplication.java
+++ src/main/java/com/tutorbooking/HomeTutorApplication.java
@@ -1,4 +1,12 @@
 package com.tutorbooking;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HomeTutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeTutorApplication.class, args);
+    }
 }
